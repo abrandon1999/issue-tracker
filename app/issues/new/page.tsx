857c4f925ebf67f61ebd7f6cf8bc74d7369c1a3c
brandon <brandon@ubuntu.myguest.virtualbox.org>
2023-10-12 18:06:42 -0400

    Handling Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,37 @@
 'use client';
 import { Button, TextArea, TextField } from '@radix-ui/themes';
 //import SimpleMDE from 'react-simplemde-editor';
-import 'easymde/dist/easymde.min.css';
+//import 'easymde/dist/easymde.min.css';
 import React from 'react';
+//import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
+interface IssueForm {
+    title: string;
+    description: string;
+}
 const NewIssuePage = () => {
+    const router = useRouter();
+    //const { register, control, handleSubmit } = useForm<IssueForm>();
+    const { register, control, handleSubmit } = useForm<IssueForm>();
+    console.log(register('title'));
     return (
-        <div className="max-w-xl space-y-3">
+        <form
+            className="max-w-xl space-y-3"
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post('/api/issues', data);
+                router.push('/issues');
+            })}
+        >
             <TextField.Root>
-                <TextField.Input placeholder="Title" />
+                <TextField.Input placeholder="Title" {...register('title')} />
             </TextField.Root>
-            <TextArea placeholder="Description" />
+            <TextArea placeholder="Description" {...register('description')} />
+
             <Button>Summit New Issue</Button>
-        </div>
+        </form>
     );
 };
 
@@ -21,3 +40,5 @@ export default NewIssuePage;
 //issue with building project,had to replace with
 //TextArea component
 //<SimpleMDE placeholder="Description" />;
+//-------------------------------------------------------------------------------------------
+//<Controller name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />;
